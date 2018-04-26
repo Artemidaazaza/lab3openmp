ce42a25419b26f186e915d090c39676c7b91eec4
Artemidaazaza <artemidaazaza@gmail.com>
2018-04-26 14:04:05 +0300

    update

--- lab3openmp/lab3openmp.cpp
+++ lab3openmp/lab3openmp.cpp
@@ -6,11 +6,11 @@
 
 int main(int argc, char* argv[]) {
 	long number, h;
-	double x, sum, startTime, resultSum;
+	double x, sumOnThread, startTime, resultSum;
 	while (true)
 	{
 		number = h = 0;
-		x = sum = startTime = resultSum = 0.0;
+		x = sumOnThread = startTime = resultSum = 0.0;
 		printf("Enter number: ");
 		scanf("%d", &number);
 		if (number <= 0) {
@@ -18,17 +18,17 @@ int main(int argc, char* argv[]) {
 		}
 		startTime = omp_get_wtime();
 		h = 2 * number;
-		resultSum = sum = 0;
+		resultSum = sumOnThread = 0;
 #pragma omp parallel firstprivate(sum) private(x) shared(number, resultSum, h)
 		{
 #pragma omp for schedule(guided, 10) nowait
 			for (int g = 0; g < number; g++) {
 				x = (2.0 * g*1.0 - 1.0) / h*1.0;
-				sum += 4.0 / (1.0 + x * x);
+				sumOnThread += 4.0 / (1.0 + x * x);
 			}
-			sum *= 1.0 / number;
+			sumOnThread *= 1.0 / number;
 #pragma omp atomic 
-			resultSum += sum;
+			resultSum += sumOnThread;
 		}
 		printf("Result is %2.15f, error of value %2.15f\nRun time in seconds: %2.6f\n", resultSum, resultSum - M_PI, omp_get_wtime() - startTime);
 	}
